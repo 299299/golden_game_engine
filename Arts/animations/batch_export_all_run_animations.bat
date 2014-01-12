78bf5440ea86cc4d48cd63d86a3108c9fc68d598
golden <magic.lixin@gmail.com>
2014-01-12 22:47:32 +0800

    pipeline update.

--- Arts/animations/batch_export_all_run_animations.bat
+++ Arts/animations/batch_export_all_run_animations.bat
@@ -1,5 +1,7 @@
 echo exporting run animations
 MayaBatchTool.exe 1_Run_Forward.mb 4 monk/animations/monk_run_0.hkx
+MayaBatchTool.exe 1_Run_Forward_Left.mb 5 monk/animations/monk_run_left.hkx
+MayaBatchTool.exe 1_Run_Forward_Right.mb 5 monk/animations/monk_run_right.hkx
 MayaBatchTool.exe 1_Run_Right_Turn_180.mb 5 monk/animations/monk_run_180.hkx
 MayaBatchTool.exe 1_Stand_To_Run.mb 4 monk/animations/monk_stand_to_run_0.hkx
 MayaBatchTool.exe 1_Stand_To_Run_Right_45.mb 5 monk/animations/monk_stand_to_run_45.hkx
