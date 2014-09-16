ca2014de1b7c8cc80ef9204f730cbc149f1139b0
lixin <li.xin@pset.suntec.net>
2014-09-16 11:45:59 +0800

    update

--- Source/Engine/Engine/Actor.h
+++ Source/Engine/Engine/Actor.h
@@ -2,14 +2,22 @@
 #include "config.h"
 #include "BaseTypes.h"
 #include "StringId.h"
+#include "DataDef.h"
 
 struct ActorResource
 {
-
+    StringId        m_componentResourceNames[kComponentTypeNum];
+    void*           m_componentResources[kComponentTypeNum];
 };
 
 
 struct Actor
 {
-    Id           m_components[kComponentTypeNum];
+    Id              m_components[kComponentTypeNum];
+};
+
+
+struct ActorWorld
+{
+    
 };
