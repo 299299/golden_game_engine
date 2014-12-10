520249adb08535f795e561c31f536c12da8e84cf
golden <magic.lixin@gmail.com>
2014-12-10 22:05:38 +0800

    script clean up

--- Pipeline/NagaMayaPipeline.py
+++ Pipeline/NagaMayaPipeline.py
@@ -1,17 +1,102 @@
-import NagaMayaUtil as NAGA
-reload(NAGA)
+import os
+import shutil
 import maya.cmds as cmds
 import maya.mel as mel
-import maya.OpenMaya as OM
-import maya.OpenMayaUI as OMUI
+import maya.api.OpenMaya as OpenMaya
 import time
-import os
+import subprocess
+import math
 
-PREVIEW_PACK = 'preview'
-GAME_APP = 'Game_Debug'
+
 UI_NAME = 'Naga_UI'
-DEBUG = True
 
+def lunchApplication(name, folder, args, wait=False):
+    print('lunch name = ' + name + ', folder = ' + folder)
+    path = folder + name
+    cmdArgs = args
+    cmdArgs.insert(0, path)
+    print(cmdArgs)
+    p = subprocess.Popen(cmdArgs, cwd=folder)
+    if(wait):
+        stdout, stderr = p.communicate()
+        print(stdout)
+        print(stderr)
+
+
+def getCameraFov(cameraShapeName='perspShape'):
+    aperture = cmds.getAttr(cameraShapeName + '.hfa')
+    fl = cmds.getAttr(cameraShapeName + '.focalLength')
+    fov = aperture * 0.5 / (fl * 0.03937)
+    fov = 2.0 * math.atan(fov) / 3.14159 * 180.0
+    return fov
+
+
+def getSceneName():
+    fileName = cmds.file(q=True, sn=True)
+    print("current scene = " + fileName)
+    fileName = os.path.basename(fileName)
+    if(not fileName):
+        cmds.warning('not named scene!')
+        return 'untitiled'
+    (prefix, sep, suffix) = fileName.rpartition('.')
+    return prefix
+
+def getSceneDirectory():
+    fileName = cmds.file(q=True, sn=True)
+    print("current scene = " + fileName)
+    return os.path.dirname(fileName)
+
+def osGetFileList(directory, ext):
+    fileList = []
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            lowerName = name.lower()
+            if lowerName.endswith(ext):
+                path = os.path.join(root, name)
+                path = path.replace('\\', '/')
+                fileList.append(path)
+    return fileList
+
+def fixAssemblyRepPath():
+    nodeList = cmds.ls(type='assemblyDefinition')
+    for node in nodeList:
+        dataAttrib = node + '.representations[0].repData'
+        data = cmds.getAttr(dataAttrib)
+        # FIXME just hack with c:/ start or d:/ start
+        if data.startswith('C:/') or data.startswith('D:/'):
+            pos = data.find('/LevelData') + 1
+            strlen = len(data)
+            data = data[pos: strlen]
+            print(data)
+            cmds.setAttr(dataAttrib, data, type='string')
+
+def fixAssemblyTransform():
+    node = getNoneCameraNode()
+    if(node == ''):
+        return
+    cmds.setAttr(node + '.translate', 0, 0, 0, type='float3')
+    cmds.setAttr(node + '.rotate', 0, 0, 0, type='float3')
+    cmds.setAttr(node + '.scale', 1, 1, 1, type='float3')
+
+def getAD_EntityType(node):
+    dataAttrib = node + '.representations[0].repData'
+    repData = cmds.getAttr(dataAttrib)
+    base = os.path.basename(repData)
+    return os.path.splitext(base)[0]
+
+
+def openAD_Reference():
+    node = cmds.ls(sl=True)[0]
+    nodeType = cmds.nodeType(node)
+    if(nodeType != 'assemblyDefinition'):
+        node = cmds.listRelatives(node, parent=True)[0]
+    repData = cmds.getAttr(node + '.representations[0].repData')
+    print(repData)
+    workSpace = cmds.workspace(q=True, dir=True, rd=True)
+    refFile = os.path.join(workSpace, repData)
+    print(refFile)
+    cmds.file(save=True)
+    cmds.file(refFile, open=True)
 
 class NagaPipeline(object):
     NagaObject = None
@@ -21,18 +106,13 @@ class NagaPipeline(object):
 
     def create(self):
         self.callbacks = []
-        # print("########### NagaPipeline::create start ##################")
-        self.naga = NAGA.NagaMayaUtil()
-        self.engineScale = 0.01
-        self.lastSyncCallback = None
-        # GUI
         self._createUI()
         self.created = True
+        self.lastMayaScene = ''
         cmds.scriptJob(parent=self.myWindow, cu=False, ro=False,
                        event=('SceneOpened', self.onSceneChanged))
         cmds.scriptJob(parent=self.myWindow, cu=False, ro=False,
                        event=('SceneSaved', self.onSceneChanged))
-        # print("########### NagaPipeline::create end ##################")
 
     def destroy(self):
         if not self.created:
@@ -54,30 +134,16 @@ class NagaPipeline(object):
         if cmds.window(UI_NAME, exists=1):
             cmds.deleteUI(UI_NAME)
 
-        NAGA.createHistoryNode()
         self.myWindow = cmds.window(UI_NAME, t='Pipeline')
         cmds.columnLayout(cal='center', adj=1)
 
         #cmds.frameLayout(l='Export Name', cll=1, h=50)
         cmds.text(label='Export Name', align='center')
-        displayName = '*** %s ***' % NAGA.getSceneName()
+        displayName = '*** %s ***' % getSceneName()
         self.nameText = cmds.text(label=displayName,
                                   align='center',
                                   bgc=[0, 1, 0],
                                   h=30)
-
-        cmds.frameLayout(l="Export Parameters", cll=1)
-        self.selectCheck = cmds.checkBox(label='Export Select Only', v=False)
-        self.hkoTypeGroup = cmds.optionMenuGrp('HkoType', l='type:')
-        for hkoType in self.naga.getHkoList():
-            hkoType = os.path.basename(hkoType)
-            cmds.menuItem(label=hkoType)
-        self.rigTypeGroup = cmds.optionMenuGrp('RigType', l='rig:')
-        for rigName in self.naga.getRigList():
-            cmds.menuItem(label=rigName)
-        self.packageGroup = cmds.optionMenuGrp('PackageName', l='package:')
-        for packageName in self.naga.getPackageList():
-            cmds.menuItem(label=packageName)
 
         cmds.setParent('..')
 
@@ -85,13 +151,9 @@ class NagaPipeline(object):
         # cmds.rowLayout(nc=2)
         cmds.button('Open Selection AR', c=self.onOpenARClicked)
         cmds.button('Back To Last Level', c=self.onBackLevelClicked)
-        cmds.button('Add Anim-Trigger', c=NAGA.createTriggerNode)
-        cmds.button('Add Proxy', c=NAGA.createProxyNode)
-        cmds.button('Add Script', c=NAGA.createScriptNode)
-        cmds.button('Add Anim Rig', c=self.onCreateRigButtonClicked)
-        cmds.button('Add Movement', c=NAGA.createMovementNode)
-        for i in [0, 7]:
-            cmds.setParent('..')
+        cmds.setParent('..')
+        cmds.setParent('..')
+
 
         height = 75
         margin_w = 5
@@ -110,7 +172,7 @@ class NagaPipeline(object):
 
     def onOpenARClicked(self, *arg):
         self.lastMayaScene = cmds.file(q=True, sn=True)
-        NAGA.openAD_Reference()
+        openAD_Reference()
 
     def onBackLevelClicked(self, *arg):
         if(not self.lastMayaScene):
@@ -119,63 +181,20 @@ class NagaPipeline(object):
         cmds.file(self.lastMayaScene, o=True)
 
     def onExportButtonClicked(self, *arg):
-        self.export(NAGA.getSceneName())
-
-    def onCreateRigButtonClicked(self, *arg):
-        NAGA.createAnimRigNode(self.naga.getRigList())
+        self.export(getSceneName())
 
     def updateUIByData(self):
         #
-        displayName = '*** %s ***' % NAGA.getSceneName()
+        displayName = '*** %s ***' % getSceneName()
         cmds.text(self.nameText, e=1, label=displayName)
-        #
-        last_hko = NAGA.readHistory('last_hko_type')
-        last_rig = NAGA.readHistory('last_rig_type')
-        last_package = NAGA.readHistory('last_package_type')
-        print('last_hko=%d' % last_hko)
-        print('last_rig=%d' % last_rig)
-        print('last_package=%d' % last_package)
-        cmds.optionMenuGrp(self.hkoTypeGroup, e=1, sl=last_hko)
-        cmds.optionMenuGrp(self.rigTypeGroup, e=1, sl=last_rig)
-        cmds.optionMenuGrp(self.packageGroup, e=1, sl=last_package)
 
     #
     # UTIL FUNCTIONS
     #
-    def doExport(self, exportName, packageName, hkoType=0, rigName=''):
-        # step1 : export use havok plugin.
-        self.naga.exportToHKX(
-            exportName, packageName, hkoType, rig=rigName, arg='',
-            s=self.isSelectOnly())
-
-        # step2 : convert the hkx to intermediate data
-        # using havok converter
-        # step3: lunch data-compiler to convert intermediate to finally package
-        self.naga.convertHkx(exportName, packageName, bDebug=DEBUG)
-
     def export(self, exportName):
-
+        pass
         #
-        packageIndex = cmds.optionMenuGrp(self.packageGroup, q=1, sl=1) - 1
-        packageName = self.naga.getPackageList()[packageIndex]
-        packageName = packageName
-        NAGA.updateHistoryNode('last_package_type', packageIndex + 1)
 
-        #
-        rigIndex = cmds.optionMenuGrp(self.rigTypeGroup, q=1, sl=1) - 1
-        rigName = self.naga.getRigList()[rigIndex]
-        rigName = os.path.basename(rigName)
-        NAGA.updateHistoryNode('last_rig_type', rigIndex + 1)
-
-        #
-        hkoIndex = cmds.optionMenuGrp(self.hkoTypeGroup, q=1, sl=1) - 1
-        NAGA.updateHistoryNode('last_hko_type', hkoIndex + 1)
-
-        #
-        self.doExport(exportName,
-                      packageName,
-                      hkoType=hkoIndex,
-                      rigName=rigName)
 
 
 def clean():
