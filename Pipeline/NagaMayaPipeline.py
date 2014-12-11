74822fad9a166a6c29441c66e6b588586326048e
lixin <li.xin@pset.suntec.net>
2014-12-11 18:01:00 +0800

    update

--- Pipeline/NagaMayaPipeline.py
+++ Pipeline/NagaMayaPipeline.py
@@ -1,14 +1,13 @@
 import os
-import shutil
 import maya.cmds as cmds
 import maya.mel as mel
-import maya.api.OpenMaya as OpenMaya
-import time
 import subprocess
 import math
+import sys
 
 
 UI_NAME = 'Naga_UI'
+
 
 def lunchApplication(name, folder, args, wait=False):
     print('lunch name = ' + name + ', folder = ' + folder)
@@ -41,10 +40,12 @@ def getSceneName():
     (prefix, sep, suffix) = fileName.rpartition('.')
     return prefix
 
+
 def getSceneDirectory():
     fileName = cmds.file(q=True, sn=True)
     print("current scene = " + fileName)
     return os.path.dirname(fileName)
+
 
 def osGetFileList(directory, ext):
     fileList = []
@@ -57,26 +58,29 @@ def osGetFileList(directory, ext):
                 fileList.append(path)
     return fileList
 
+
 def fixAssemblyRepPath():
     nodeList = cmds.ls(type='assemblyDefinition')
     for node in nodeList:
         dataAttrib = node + '.representations[0].repData'
         data = cmds.getAttr(dataAttrib)
-        # FIXME just hack with c:/ start or d:/ start
-        if data.startswith('C:/') or data.startswith('D:/'):
-            pos = data.find('/LevelData') + 1
-            strlen = len(data)
-            data = data[pos: strlen]
-            print(data)
-            cmds.setAttr(dataAttrib, data, type='string')
+        # FIXME just hack with c:/ d:/ ... h:/ start
+        disk_list = ['C', 'D', 'E', 'F', 'G', 'H']
+        for disk in disk_list:
+            if data.toLower().startswith(disk):
+                pos = data.find('/LevelData') + 1
+                strlen = len(data)
+                data = data[pos: strlen]
+                print(data)
+                cmds.setAttr(dataAttrib, data, type='string')
+
 
 def fixAssemblyTransform():
-    node = getNoneCameraNode()
-    if(node == ''):
-        return
+    node = cmds.ls(sl=True)[0]
     cmds.setAttr(node + '.translate', 0, 0, 0, type='float3')
     cmds.setAttr(node + '.rotate', 0, 0, 0, type='float3')
     cmds.setAttr(node + '.scale', 1, 1, 1, type='float3')
+
 
 def getAD_EntityType(node):
     dataAttrib = node + '.representations[0].repData'
@@ -98,6 +102,40 @@ def openAD_Reference():
     cmds.file(save=True)
     cmds.file(refFile, open=True)
 
+
+def fbx_export(output_filename):
+    mel.eval("FBXExportSmoothingGroups -v true")
+    #mel.eval("FBXExportHardEdges -v false")
+    #mel.eval("FBXExportTangents -v false")
+    mel.eval("FBXExportSmoothMesh -v true")
+    #mel.eval("FBXExportInstances -v false")
+    #mel.eval("FBXExportReferencedContainersContent -v false")
+    # Animation
+    #mel.eval("FBXExportBakeComplexAnimation -v true")
+    #mel.eval("FBXExportBakeComplexStart -v "+str(exportStartFrame[x]))
+    #mel.eval("FBXExportBakeComplexEnd -v "+str(exportEndFrame[x]))
+    #mel.eval("FBXExportBakeComplexStep -v 1")
+    # mm.eval("FBXExportBakeResampleAll -v true")
+    #mel.eval("FBXExportUseSceneName -v false")
+    #mel.eval("FBXExportQuaternion -v euler")
+    mel.eval("FBXExportShapes -v true")
+    mel.eval("FBXExportSkins -v true")
+    # Constraints
+    mel.eval("FBXExportConstraints -v false")
+    # Cameras
+    mel.eval("FBXExportCameras -v true")
+    # Lights
+    mel.eval("FBXExportLights -v true")
+    # Embed Media
+    #mel.eval("FBXExportEmbeddedTextures -v true")
+    # Connections
+    #mel.eval("FBXExportInputConnections -v false")
+    # Axis Conversion
+    mel.eval("FBXExportUpAxis y")
+    # Export!
+    mel.eval("FBXExport -f " + output_filename + ".fbx -s")
+
+
 class NagaPipeline(object):
     NagaObject = None
 
@@ -113,6 +151,9 @@ class NagaPipeline(object):
                        event=('SceneOpened', self.onSceneChanged))
         cmds.scriptJob(parent=self.myWindow, cu=False, ro=False,
                        event=('SceneSaved', self.onSceneChanged))
+        self.nagaDir = os.environ['NAGA_DIR']
+        self.binDir = self.nagaDir + '/Application'
+        self.dataDir = self.binDir + '/Data'
 
     def destroy(self):
         if not self.created:
@@ -137,7 +178,6 @@ class NagaPipeline(object):
         self.myWindow = cmds.window(UI_NAME, t='Pipeline')
         cmds.columnLayout(cal='center', adj=1)
 
-        #cmds.frameLayout(l='Export Name', cll=1, h=50)
         cmds.text(label='Export Name', align='center')
         displayName = '*** %s ***' % getSceneName()
         self.nameText = cmds.text(label=displayName,
@@ -147,13 +187,20 @@ class NagaPipeline(object):
 
         cmds.setParent('..')
 
-        cmds.frameLayout(l="Util", cll=1)
+        self.exportTypeGrp = cmds.optionMenuGrp('ExportType', l='type:')
+        cmds.menuItem(label='model')
+        cmds.menuItem(label='scene')
+        cmds.menuItem(label='animation')
+        cmds.setParent('..')
+
+        cmds.frameLayout(l="Assembly Util", cll=1)
         # cmds.rowLayout(nc=2)
+        cmds.button('Fix Assembly Path', c=self.onFixPathClicked)
         cmds.button('Open Selection AR', c=self.onOpenARClicked)
         cmds.button('Back To Last Level', c=self.onBackLevelClicked)
         cmds.setParent('..')
         cmds.setParent('..')
-
+        cmds.setParent('..')
 
         height = 75
         margin_w = 5
@@ -164,11 +211,13 @@ class NagaPipeline(object):
                     c=self.onExportButtonClicked)
         cmds.setParent('..')
         cmds.showWindow(UI_NAME)
-        #
         self.updateUIByData()
 
     def isSelectOnly(self):
         return cmds.checkBox(self.selectCheck, q=1, v=1)
+
+    def onFixPathClicked(self):
+        fixAssemblyRepPath()
 
     def onOpenARClicked(self, *arg):
         self.lastMayaScene = cmds.file(q=True, sn=True)
@@ -179,6 +228,7 @@ class NagaPipeline(object):
             return
         cmds.file(mf=0)
         cmds.file(self.lastMayaScene, o=True)
+        self.lastMayaScene = ''
 
     def onExportButtonClicked(self, *arg):
         self.export(getSceneName())
@@ -188,13 +238,15 @@ class NagaPipeline(object):
         displayName = '*** %s ***' % getSceneName()
         cmds.text(self.nameText, e=1, label=displayName)
 
+    def getExportType(self):
+        return cmds.optionMenuGrp(self.exportTypeGrp, v=1, sl=1)
     #
     # UTIL FUNCTIONS
     #
+
     def export(self, exportName):
         pass
         #
-
 
 
 def clean():
--- Pipeline/pipeline_copy.bat
+++ Pipeline/pipeline_copy.bat
@@ -4,4 +4,4 @@ set "maya_shelf_dir=%maya_user_dir%2014-x64\prefs\shelves\"
 
 ::copy python scirpts
 copy *.py                       %maya_script_dir%
-copy shelf_Custom.mel           %maya_script_dir%
+copy *.mel                      %maya_script_dir%
