0966a61d51c6133d035c32d6a6163bfe117d8505
lixin <li.xin@pset.suntec.net>
2014-12-12 15:39:20 +0800

    update pipeline

--- Pipeline/NagaMayaPipeline.py
+++ Pipeline/NagaMayaPipeline.py
@@ -1,139 +1,11 @@
 import os
 import maya.cmds as cmds
 import maya.mel as mel
-import subprocess
-import math
-import sys
+import Utils as utils
+reload(utils)
 
 
 UI_NAME = 'Naga_UI'
-
-
-def lunchApplication(name, folder, args, wait=False):
-    print('lunch name = ' + name + ', folder = ' + folder)
-    path = folder + name
-    cmdArgs = args
-    cmdArgs.insert(0, path)
-    print(cmdArgs)
-    p = subprocess.Popen(cmdArgs, cwd=folder)
-    if(wait):
-        stdout, stderr = p.communicate()
-        print(stdout)
-        print(stderr)
-
-
-def getCameraFov(cameraShapeName='perspShape'):
-    aperture = cmds.getAttr(cameraShapeName + '.hfa')
-    fl = cmds.getAttr(cameraShapeName + '.focalLength')
-    fov = aperture * 0.5 / (fl * 0.03937)
-    fov = 2.0 * math.atan(fov) / 3.14159 * 180.0
-    return fov
-
-
-def getSceneName():
-    fileName = cmds.file(q=True, sn=True)
-    print("current scene = " + fileName)
-    fileName = os.path.basename(fileName)
-    if(not fileName):
-        cmds.warning('not named scene!')
-        return ''
-    (prefix, sep, suffix) = fileName.rpartition('.')
-    return prefix
-
-
-def getSceneDirectory():
-    fileName = cmds.file(q=True, sn=True)
-    print("current scene = " + fileName)
-    return os.path.dirname(fileName)
-
-
-def osGetFileList(directory, ext):
-    fileList = []
-    for root, dirs, files in os.walk(directory):
-        for name in files:
-            lowerName = name.lower()
-            if lowerName.endswith(ext):
-                path = os.path.join(root, name)
-                path = path.replace('\\', '/')
-                fileList.append(path)
-    return fileList
-
-
-def fixAssemblyRepPath():
-    nodeList = cmds.ls(type='assemblyDefinition')
-    for node in nodeList:
-        dataAttrib = node + '.representations[0].repData'
-        data = cmds.getAttr(dataAttrib)
-        # FIXME just hack with c:/ d:/ ... h:/ start
-        disk_list = ['C', 'D', 'E', 'F', 'G', 'H']
-        for disk in disk_list:
-            if data.toLower().startswith(disk):
-                pos = data.find('/LevelData') + 1
-                strlen = len(data)
-                data = data[pos: strlen]
-                print(data)
-                cmds.setAttr(dataAttrib, data, type='string')
-
-
-def fixAssemblyTransform():
-    node = cmds.ls(sl=True)[0]
-    cmds.setAttr(node + '.translate', 0, 0, 0, type='float3')
-    cmds.setAttr(node + '.rotate', 0, 0, 0, type='float3')
-    cmds.setAttr(node + '.scale', 1, 1, 1, type='float3')
-
-
-def getAD_EntityType(node):
-    dataAttrib = node + '.representations[0].repData'
-    repData = cmds.getAttr(dataAttrib)
-    base = os.path.basename(repData)
-    return os.path.splitext(base)[0]
-
-
-def openAD_Reference():
-    node = cmds.ls(sl=True)[0]
-    nodeType = cmds.nodeType(node)
-    if(nodeType != 'assemblyDefinition'):
-        node = cmds.listRelatives(node, parent=True)[0]
-    repData = cmds.getAttr(node + '.representations[0].repData')
-    print(repData)
-    workSpace = cmds.workspace(q=True, dir=True, rd=True)
-    refFile = os.path.join(workSpace, repData)
-    print(refFile)
-    cmds.file(save=True)
-    cmds.file(refFile, open=True)
-
-
-def fbx_export(output_filename):
-    mel.eval("FBXExportSmoothingGroups -v true")
-    #mel.eval("FBXExportHardEdges -v false")
-    #mel.eval("FBXExportTangents -v false")
-    mel.eval("FBXExportSmoothMesh -v true")
-    #mel.eval("FBXExportInstances -v false")
-    #mel.eval("FBXExportReferencedContainersContent -v false")
-    # Animation
-    #mel.eval("FBXExportBakeComplexAnimation -v true")
-    #mel.eval("FBXExportBakeComplexStart -v "+str(exportStartFrame[x]))
-    #mel.eval("FBXExportBakeComplexEnd -v "+str(exportEndFrame[x]))
-    #mel.eval("FBXExportBakeComplexStep -v 1")
-    # mm.eval("FBXExportBakeResampleAll -v true")
-    #mel.eval("FBXExportUseSceneName -v false")
-    #mel.eval("FBXExportQuaternion -v euler")
-    mel.eval("FBXExportShapes -v true")
-    mel.eval("FBXExportSkins -v true")
-    # Constraints
-    mel.eval("FBXExportConstraints -v false")
-    # Cameras
-    mel.eval("FBXExportCameras -v true")
-    # Lights
-    mel.eval("FBXExportLights -v true")
-    # Embed Media
-    #mel.eval("FBXExportEmbeddedTextures -v true")
-    # Connections
-    #mel.eval("FBXExportInputConnections -v false")
-    # Axis Conversion
-    mel.eval("FBXExportUpAxis y")
-    # Export!
-    mel.eval("FBXExport -f " + output_filename + ".fbx -s")
 
 
 class NagaPipeline(object):
@@ -152,8 +24,16 @@ class NagaPipeline(object):
         cmds.scriptJob(parent=self.myWindow, cu=False, ro=False,
                        event=('SceneSaved', self.onSceneChanged))
         self.nagaDir = os.environ['NAGA_DIR']
-        self.binDir = self.nagaDir + '\Application'
-        self.dataDir = self.binDir + '\Data'
+        if not self.nagaDir:
+            print('set NAGA_DIR environment first!.')
+            cmds.error('!!!!')
+        self.nagaDir.replace('\\', '/')
+        self.binDir = self.nagaDir + '/Application/'
+        self.dataDir = self.binDir + '/Data/'
+        self.fbxDir = self.binDir + '/FBX/'
+        # create fbx folder if not exist
+        if not os.path.exists(self.fbxDir):
+            os.makedirs(self.fbxDir)
 
     def destroy(self):
         if not self.created:
@@ -179,7 +59,7 @@ class NagaPipeline(object):
         cmds.columnLayout(cal='center', adj=1)
 
         cmds.text(label='Export Name', align='center')
-        displayName = '*** %s ***' % getSceneName()
+        displayName = '*** %s ***' % utils.getSceneName()
         self.nameText = cmds.text(label=displayName,
                                   align='center',
                                   bgc=[0, 1, 0],
@@ -209,6 +89,8 @@ class NagaPipeline(object):
             l="Export", cll=1, mh=margin_h, mw=margin_w)
         cmds.button('EXPORT', h=height, bgc=[0, 1, 1],
                     c=self.onExportButtonClicked)
+        cmds.button('PREVIEW', h=height, bgc=[1, 0, 1],
+                    c=self.onPreviewButtonClicked)
         cmds.setParent('..')
         cmds.showWindow(UI_NAME)
         self.updateUIByData()
@@ -217,11 +99,11 @@ class NagaPipeline(object):
         return cmds.checkBox(self.selectCheck, q=1, v=1)
 
     def onFixPathClicked(self, *arg):
-        fixAssemblyRepPath()
+        utils.fixAssemblyRepPath()
 
     def onOpenARClicked(self, *arg):
         self.lastMayaScene = cmds.file(q=True, sn=True)
-        openAD_Reference()
+        utils.openAD_Reference()
 
     def onBackLevelClicked(self, *arg):
         if(self.lastMayaScene == ''):
@@ -231,14 +113,17 @@ class NagaPipeline(object):
         self.lastMayaScene = ''
 
     def onExportButtonClicked(self, *arg):
-        name = getSceneName()
+        name = utils.getSceneName()
         if name == '':
             return
         self.export(name)
 
+    def onPreviewButtonClicked(self, *arg):
+        self.preview()
+
     def updateUIByData(self):
         #
-        name = getSceneName()
+        name = utils.getSceneName()
         if name == '':
             name = 'untitiled'
         displayName = '*** %s ***' % name
@@ -246,13 +131,34 @@ class NagaPipeline(object):
 
     def getExportType(self):
         return cmds.optionMenuGrp(self.exportTypeGrp, v=1, sl=1)
-    #
-    # UTIL FUNCTIONS
-    #
 
     def export(self, exportName):
-        pass
-        #
+        fbxFile = self.fbxDir + exportName + '.fbx'
+        utils.fbx_export(fbxFile)
+        if not os.path.exists(fbxFile):
+            print('export error')
+            return
+        eType = self.getExportType()
+        toolFile = self.binDir + 'NagaTool'
+        outputDir = self.dataDir + exportName + '/'
+        extName = 'xml'
+        if eType == 'model':
+            extName = 'mdl'
+        outputFile = outputDir + exportName + '.' + extName
+        args = [eType, fbxFile, outputFile, '-t', '-ns', '-v']
+        utils.mkdir(outputDir)
+        utils.lunchApplication(toolFile, self.binDir, args)
+        self.lastOutputFile = outputFile
+        self.lastFBXFile = fbxFile
+
+    def preview(self):
+        if not self.lastOutputFile:
+            return
+        appFile = self.binDir + 'NagaGame'
+        scriptFile = 'Data/Scripts/Viewer.as'
+        eType = self.getExportType()
+        args = [scriptFile, '-w', eType, self.lastOutputFile]
+        utils.lunchApplication(appFile, self.binDir, args, False)
 
 
 def clean():
---
+++ Pipeline/Utils.py
@@ -0,0 +1,147 @@
+import os
+import maya.cmds as cmds
+import maya.mel as mel
+import subprocess
+
+
+def lunchApplication(name, folder, args, wait=False):
+    print('lunch name = ' + name + ', folder = ' + folder)
+    path = folder + name
+    cmdArgs = args
+    cmdArgs.insert(0, path)
+    print(cmdArgs)
+    p = subprocess.Popen(cmdArgs, cwd=folder)
+    if(wait):
+        stdout, stderr = p.communicate()
+        print(stdout)
+        print(stderr)
+
+
+def getSceneName():
+    fileName = cmds.file(q=True, sn=True)
+    print("current scene = " + fileName)
+    fileName = os.path.basename(fileName)
+    if(not fileName):
+        cmds.warning('not named scene!')
+        return ''
+    (prefix, sep, suffix) = fileName.rpartition('.')
+    return prefix
+
+
+def getSceneDirectory():
+    fileName = cmds.file(q=True, sn=True)
+    print("current scene = " + fileName)
+    return os.path.dirname(fileName)
+
+
+def osGetFileList(directory, ext):
+    fileList = []
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            lowerName = name.lower()
+            if lowerName.endswith(ext):
+                path = os.path.join(root, name)
+                path = path.replace('\\', '/')
+                fileList.append(path)
+    return fileList
+
+
+def fixAssemblyRepPath():
+    nodeList = cmds.ls(type='assemblyDefinition')
+    for node in nodeList:
+        dataAttrib = node + '.representations[0].repData'
+        data = cmds.getAttr(dataAttrib)
+        # FIXME just hack with c:/ d:/ ... h:/ start
+        disk_list = ['C', 'D', 'E', 'F', 'G', 'H']
+        for disk in disk_list:
+            if data.toLower().startswith(disk):
+                pos = data.find('/LevelData') + 1
+                strlen = len(data)
+                data = data[pos: strlen]
+                print(data)
+                cmds.setAttr(dataAttrib, data, type='string')
+
+
+def fixAssemblyTransform():
+    node = cmds.ls(sl=True)[0]
+    cmds.setAttr(node + '.translate', 0, 0, 0, type='float3')
+    cmds.setAttr(node + '.rotate', 0, 0, 0, type='float3')
+    cmds.setAttr(node + '.scale', 1, 1, 1, type='float3')
+
+
+def getAD_EntityType(node):
+    dataAttrib = node + '.representations[0].repData'
+    repData = cmds.getAttr(dataAttrib)
+    base = os.path.basename(repData)
+    return os.path.splitext(base)[0]
+
+
+def openAD_Reference():
+    node = cmds.ls(sl=True)[0]
+    nodeType = cmds.nodeType(node)
+    if(nodeType != 'assemblyDefinition'):
+        node = cmds.listRelatives(node, parent=True)[0]
+    repData = cmds.getAttr(node + '.representations[0].repData')
+    print(repData)
+    workSpace = cmds.workspace(q=True, dir=True, rd=True)
+    refFile = os.path.join(workSpace, repData)
+    print(refFile)
+    cmds.file(save=True)
+    cmds.file(refFile, open=True)
+
+
+def fbx_export(outFile):
+    print('fbx_export = ' + outFile)
+    mel.eval("FBXExportSmoothingGroups -v true")
+    #mel.eval("FBXExportHardEdges -v false")
+    #mel.eval("FBXExportTangents -v false")
+    mel.eval("FBXExportSmoothMesh -v true")
+    #mel.eval("FBXExportInstances -v false")
+    #mel.eval("FBXExportReferencedContainersContent -v false")
+    # Animation
+    #mel.eval("FBXExportBakeComplexAnimation -v true")
+    #mel.eval("FBXExportBakeComplexStart -v "+str(exportStartFrame[x]))
+    #mel.eval("FBXExportBakeComplexEnd -v "+str(exportEndFrame[x]))
+    #mel.eval("FBXExportBakeComplexStep -v 1")
+    # mm.eval("FBXExportBakeResampleAll -v true")
+    #mel.eval("FBXExportUseSceneName -v false")
+    #mel.eval("FBXExportQuaternion -v euler")
+    mel.eval("FBXExportShapes -v true")
+    mel.eval("FBXExportSkins -v true")
+    # Constraints
+    mel.eval("FBXExportConstraints -v false")
+    # Cameras
+    mel.eval("FBXExportCameras -v true")
+    # Lights
+    mel.eval("FBXExportLights -v true")
+    # Embed Media
+    #mel.eval("FBXExportEmbeddedTextures -v true")
+    # Connections
+    #mel.eval("FBXExportInputConnections -v false")
+    # Axis Conversion
+    mel.eval("FBXExportUpAxis y")
+    # Export!
+    mel.eval("FBXExport -f " + outFile + ".fbx -s")
+
+
+def mkdir(path):
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+    path = path.rstrip("/")
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        print(path + ' 创建成功')
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print(path + ' 目录已存在')
+        return False
